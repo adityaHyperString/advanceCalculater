13030ecc8a6d4a539ed8596788c5bd857c8f9dd5
Aditya <adisawant276@gmail.com>
2022-04-07 18:28:33 +0530

    changes done

--- calculator.js
+++ calculator.js
@@ -1,67 +1,72 @@
-console.log("calculator.js");
 
-let displayScreen = document.getElementById('displayScreen');
-// let btn = document.getElementsByClassName('btn');
-let btn = Array.from(document.getElementsByClassName('btn'));
+const displayScreen = document.getElementById('displayScreen');
+const btn = Array.from(document.getElementsByClassName('btn'));
 
-console.log(btn);
+// console.log(btn);
 
-btn.map(btn =>{
-    btn.addEventListener('click', (e) =>{
-       
-        // console.log('clicked on',e);
-        // console.log('clicked on',e.target);
-        console.log('clicked on',e.target.innerText);
-      let selectedChar = e.target.innerText  
+btn.map(btn => {
+    btn.addEventListener('click', (e) => {
+        // console.log('clicked on', e.target.innerText);
+        let selectedChar = e.target.innerText.toLowerCase();
         switch (selectedChar) {
             case '=':
-                displayScreen.innerText = eval(displayScreen.innerText);
-                break;   
+                try {
+                    displayScreen.innerText = eval(displayScreen.innerText);
+                } catch (error) {
+                    alert('please enter proper values');
+                    displayScreen.innerText='';
+                }
+                break;
 
-            case 'C':
+            case 'c':
                 displayScreen.innerText = '';
                 break;
             case '←':
-                displayScreen.innerText = displayScreen.innerText.slice(0,-1);
-                break;    
+                displayScreen.innerText = displayScreen.innerText.slice(0, -1);
+                break;
             case 'x2':
                 displayScreen.innerText = (displayScreen.innerText * displayScreen.innerText);
-                break;  
+                break;
             case '√':
                 displayScreen.innerText = Math.sqrt(displayScreen.innerText);
-                break;  
+                break;
             case 'π':
                 let pieValue = 3.14159265359
                 displayScreen.innerText += pieValue;
-                break;  
+                break;
             case 'log':
                 displayScreen.innerText = Math.log(displayScreen.innerText);
-                break;   
+                break;
             case 'sin':
                 displayScreen.innerText = Math.sin(displayScreen.innerText);
-                break;   
+                break;
             case 'cos':
                 displayScreen.innerText = Math.cos(displayScreen.innerText);
                 break;
             case 'tan':
                 displayScreen.innerText = Math.tan(displayScreen.innerText);
-                break;  
+                break;
             case '10x':
-                displayScreen.innerText = Math.pow(10,displayScreen.innerText);
-                break;    
+                displayScreen.innerText = Math.pow(10, displayScreen.innerText);
+                break;
             case 'n!':
-                let ans=1;
-                if(displayScreen.innerText == 0 || displayScreen.innerText == 1){
-                    ans=1;
+                let ans = 1;
+                if (displayScreen.innerText == 0 || displayScreen.innerText == 1) {
+                    ans = 1;
                     displayScreen.innerText = ans;
-                }else{
+                } else {
                     for (let i = displayScreen.innerText; i >= 1; i--) {
-                       ans = ans * i;   
+                        ans = ans * i;
                     }
                     displayScreen.innerText = ans;
-                } 
-                break;                
+                }
+                break;
             default:
+                if (displayScreen.innerText == '') {
+                    if(displayScreen.innerText == '/' || displayScreen.innerText == '*' || displayScreen.innerText == '%' || displayScreen.innerText == '√' ){
+                        displayScreen.innerText = '';
+                    }
+                }
                 displayScreen.innerText += selectedChar
                 break;
         }
@@ -69,35 +74,3 @@ btn.map(btn =>{
 });
 
 
-// for(var i=0;i<btn.length;i+=1){
-//     if(btn[i].innerHTML === '='){
-//           btn[i].addEventListener("click", calculate(i));
-//     }
-// }
-
-
-// clear.onclick = function () {
-// screen.innerHTML = '';
-// }; 
-// function calculate(i) {
-//     return function () {
-//         screen.innerHTML = eval(screen.innerHTML);
-//     };
-//   }
-
- //   if (selectedChar == true) {
-    //     displayScreen.innerText += selectedChar
-    //     if (selectedChar == 'C') {
-    //         displayScreen.innerText = '';
-    //     }else if(selectedChar == '='){
-    //         displayScreen.innerText = eval(displayScreen.innerText);
-    //     }else if(selectedChar == '←'){
-    //         displayScreen.innerText = displayScreen.innerText.slice(0,-1);
-    //     }
-
-    //   }
-
-
-
-
-//if I enter wrong data it is not showing err how to do that
